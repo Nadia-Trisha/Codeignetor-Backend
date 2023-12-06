6eb441697690c0cfa9fadfd01c0ddd7c91f1e449
shifatrojoni <shifatrojoni@gmail.com>
2023-12-06 13:55:31 +0600

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,22 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'AdminHome::index');
+// $routes->get('/about', 'about::index');
+// $routes->get('/contact', 'contact::index');
+// $routes->get('/services', 'services::index');
+$routes->get('products', 'ProductController::index');
+$routes->get('create', 'ProductController::create');
+$routes->post('store', 'ProductController::store');
+$routes->get('products/delete/(:num)', 'ProductController::delete/$1');
+$routes->get('products/edit/(:num)', 'ProductController::edit/$1');
+$routes->put('products/update/(:num)', 'ProductController::update/$1');
+
+//signup
+$routes->get('signup', 'SignupController::index');
+$routes->match(['get','post'],'signup/store','SignupController::store');
+
