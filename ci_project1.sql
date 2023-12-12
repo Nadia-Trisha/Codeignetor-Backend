87dfe9c5322bd3ef788ddab03e67b5bb91780edb
shifatrojoni <shifatrojoni@gmail.com>
2023-12-12 11:45:26 +0600

    project

--- ci_project1.sql
+++ ci_project1.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 04, 2023 at 04:18 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 7.4.33
+-- Generation Time: Dec 12, 2023 at 06:42 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `ci_project1`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(5) UNSIGNED NOT NULL,
+  `category` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `category`) VALUES
+(1, 'flower'),
+(2, 'nabil'),
+(3, 'masum'),
+(4, 'rojoni'),
+(5, 'cloths');
 
 -- --------------------------------------------------------
 
@@ -42,22 +64,8 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`, `batch`) VALUES
-(1, '2023-12-02-065616', 'App\\Database\\Migrations\\Products1Migration', 'default', 'App', 1701501155, 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `product1`
---
-
-CREATE TABLE `product1` (
-  `product_id` int(5) UNSIGNED NOT NULL,
-  `product` varchar(100) NOT NULL,
-  `category` varchar(100) NOT NULL,
-  `price` int(11) NOT NULL,
-  `sku` varchar(50) NOT NULL,
-  `model` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+(4, '2023-12-02-065616', 'App\\Database\\Migrations\\Products1Migration', 'default', 'App', 1702187023, 1),
+(5, '2023-12-09-043514', 'App\\Database\\Migrations\\CreateCategoryTable', 'default', 'App', 1702187023, 1);
 
 -- --------------------------------------------------------
 
@@ -66,31 +74,58 @@ CREATE TABLE `product1` (
 --
 
 CREATE TABLE `products` (
-  `product_id` int(11) NOT NULL,
+  `id` int(5) UNSIGNED NOT NULL,
+  `category_id` int(5) UNSIGNED NOT NULL,
   `product` varchar(100) NOT NULL,
-  `category` varchar(50) NOT NULL,
-  `price` int(11) DEFAULT NULL,
-  `sku` varchar(50) DEFAULT NULL,
-  `model` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `price` text NOT NULL,
+  `sku` varchar(50) NOT NULL,
+  `model` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`product_id`, `product`, `category`, `price`, `sku`, `model`) VALUES
-(1, 'Masum', 'Male', 1000, 'masum_wdpf', 'Male'),
-(2, 'Xiaomi Monitor', 'Monitor', 19200, 'xiaomi_2k_monitor', '22inchi_xiaomi'),
-(3, 'Laptop', 'Car', 35000, 'hp745_30k', 'HP 754'),
-(4, 'Masum', 'Truck', 0, '', 'aa'),
-(5, 'My Product', 'Bus', 200000, 'ak1j', 'AK1J'),
-(6, '7UP', 'Truck', 98756, 'sss', '777'),
-(7, 'Audi', 'Car', 45000, 'a8Audi', 'A8'),
-(8, 'BMW', 'Car', 55000, 'x8BMW', 'X8');
+INSERT INTO `products` (`id`, `category_id`, `product`, `price`, `sku`, `model`) VALUES
+(1, 0, 'olk', '564', '74ijk', 'ioj654'),
+(2, 0, 'Transport', '9000087', '98765434', '9088'),
+(3, 0, 'Transport', '9000087', '98765434', '23A'),
+(4, 0, 'Transport', '9000087', 'ert', '23A'),
+(5, 1, 'sun', '900008', 'ert', '9088'),
+(6, 1, 'dasert', '564', 'jhjhuy', '9088'),
+(7, 1, 'Transport', '45696', '78yh', 'kjo877'),
+(8, 5, 'Shirt', '1700', 'oiy', 'wu');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) DEFAULT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `password` varchar(150) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`) VALUES
+(1, 'nadia', 'nadia@gmail.com', '$2y$10$r1mVCrkOJnxoM34h3p0e0eg66x5wFzTHa9EE8ukIfrOpXiZpWBvG6', '2023-12-07 07:43:59');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `migrations`
@@ -99,38 +134,44 @@ ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `product1`
---
-ALTER TABLE `product1`
-  ADD PRIMARY KEY (`product_id`);
-
---
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`product_id`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `product1`
---
-ALTER TABLE `product1`
-  MODIFY `product_id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
